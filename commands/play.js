cbbfcfacecdf3ff44b7bcadaaa29eab45a2e33bb
Cartofel <94912929+soc1ty@users.noreply.github.com>
2022-03-30 12:52:02 +0300

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,19 @@
 module.exports = async (bot,message,args,argsF) => {
 
-        let queue = client.player.createQueue(message.guild.id);
-        await queue.join(message.member.voice.channel);
-        let song = await queue.play(args.join(' ')).catch(_ => {
-            if(!guildQueue)
-                queue.stop();
-        });
-    };
+    if(!message.member.voice.channel) return message.channel.send({
+        embeds: [
+            {    
+                title: "Error",
+                description:`You are not in the voice channel`,
+                color: "RED"
+            }
+        ]
+    });
+
+    let queue = bot.player.createQueue(message.guild.id);
+    await queue.join(message.member.voice.channel);
+    let song = await queue.play(args.join(' ')).catch(_ => {
+        if(!guildQueue) queue.stop();
+    });
+};
 module.exports.names = ["play", "p", "плэй"];
