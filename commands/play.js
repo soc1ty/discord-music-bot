14abd43562535aab1ce6e8a0896c3926c3bfcdf7
猫头鹰 <94912929+soc1ty@users.noreply.github.com>
2022-06-08 20:55:43 +0300

    Add files via upload

--- commands/play.js
+++ commands/play.js
@@ -10,10 +10,22 @@ module.exports = async (bot,message,args,argsF) => {
         ]
     });
 
+    let guildQueue = bot.player.getQueue(message.guild.id);
+
+    message.channel.send({
+        embeds: [
+            {
+                title: "Music is loaded",
+                color: "RED"
+            }
+        ]
+    })
+
     let queue = bot.player.createQueue(message.guild.id);
     await queue.join(message.member.voice.channel);
     let song = await queue.play(args.join(' ')).catch(_ => {
         if(!guildQueue) queue.stop();
     });
+    
 };
 module.exports.names = ["play", "p", "плэй"];
